4608073ac879d8a74a26d4498d3688f27f82bca9
SAI MALAGAV <sainani7868@gmail.com>
2026-07-30 10:13:12 +0530

    Add insertion sort implementation in insertionSort.cpp
    Implement insertion sort algorithm and display sorted array

---
+++ insertionSort.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void insertionSort(int arr[], int n){
+for(int i=1; i<n; i++){
+    int curr = arr[i];
+    int prev = i-1;
+  while(prev>=0 && arr[prev]>curr){
+    arr[prev+1] = arr[prev];
+    prev--;
+  }
+  arr[prev+1] = curr;
+}
+cout<<"the sorted array:";
+for(int i=0; i<n; i++)
+cout<<arr[i]<<" ";
+}
+
+
+int main(){
+    int arr[] = {6,3,8,2,9,1,5,8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr , n);
+}

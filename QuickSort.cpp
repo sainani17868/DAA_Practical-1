cc0ea78c42e97cdde7c8aee7bc6ebaeb748aa907
SAI MALAGAV <sainani7868@gmail.com>
2026-07-31 16:07:03 +0530

    Update QuickSort.cpp

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -22,3 +22,12 @@ void quickSort(int arr[], int low, int high) {
         quickSort(arr, pivotIndex + 1, high);
     }
 }
+int main() {
+  int arr[] = {6,3,8,2,9,1,5,8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quickSort(arr, 0, n - 1);
+    cout << "the Sorted Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+}
